ea32007b7e4365dcb8904b8e73dfd1faa5291730
Sergey Ivanov <yeezusc322@gmail.com>
2023-01-05 19:37:34 +0300

    fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -55,7 +55,9 @@ const Home = () => {
                     id={obj._id}
                     title={obj.title}
                     text={obj.text}
-                    imageUrl={obj.imageUrl ? `${process.env.REACT_APP_API_URL}${obj.imageUrl}` : ''}
+                    imageUrl={
+                      obj.imageUrl ? `${process.env.REACT_APP_API_URL}/${obj.imageUrl}` : ''
+                    }
                     user={obj.user}
                     createdAt={obj.createdAt}
                     viewsCount={obj.viewsCount}
